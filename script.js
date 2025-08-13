1ba67fdc8f20ee99128cf52ac823325b37d3874f
RamyGarici <ramymanilgarici@gmail.com>
2025-08-13 16:12:09 +0100

    add books finished

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.querySelectorAll('.book-box img').forEach(img => {
     document.getElementById('modal-author').textContent = "Author : " + img.dataset.author;
     document.getElementById('modal-description').textContent = img.dataset.description;
 
-    document.getElementById('book-modal').style.display = 'block';
+    document.getElementById('book-modal').style.display = 'flex';
   });
 });
 
@@ -22,20 +22,45 @@ window.addEventListener('click', (e) => {
 
 
 //Add function 
-function handleAddButtonClick(name,author,description,url){
+function handleAddButtonClick(name, author, description, url) {
+    console.log("Add button clicked with values:", name, author, description, url);
+
     let bookContainer = document.getElementsByClassName("book-boxes")[0];
     let newBookBox = document.createElement("div");
     newBookBox.className = "book-box";
+
     let newBookImage = document.createElement("img");
     newBookImage.dataset.title = name;
     newBookImage.dataset.author = author;
     newBookImage.dataset.description = description;
     newBookImage.src = url;
-    newBookBox.appendChild(newBookImage);
 
+
+    newBookImage.addEventListener('click', () => {
+        document.getElementById('modal-title').textContent = name;
+        document.getElementById('modal-author').textContent = "Author : " + author;
+        document.getElementById('modal-description').textContent = description;
+        document.getElementById('book-modal').style.display = 'flex';
+    });
+
+    newBookBox.appendChild(newBookImage);
+    bookContainer.appendChild(newBookBox);
     
+   document.getElementById('book-add').style.display = 'none';
+
+
 }
 
+
 const addButton = document.getElementById('add-button');
+addButton.addEventListener('click', () => {
+    document.getElementById('book-add').style.display = 'flex';
+
+    
+})
+document.getElementById('close-add').addEventListener('click', () => {
+   
+  document.getElementById('book-add').style.display = 'none';
+});
 
 
